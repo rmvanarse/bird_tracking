0f4fcca48957585f03328b29a646e229c0c4030f
Rishikesh Vanarse <rishikesh.vanarse@gmail.com>
2019-05-29 23:02:29 +0530

    Audio rec by mic

---
+++ raspi_codes/record.py
@@ -0,0 +1,31 @@
+"""
+Author: Rishikesh Vanarse
+
+Record audio and sav to a wav file
+Requires: souddevice, scipy
+"""
+
+
+import sounddevice as sd
+import numpy as np 
+import scipy.io.wavfile as wav 
+
+
+filename = 'recorded_audio'
+sample_rate = 40000
+duration = 5 #No. of seconds
+
+recording = sd.rec(duration * sample_rate, samplerate = sample_rate, channels =2, dtype = 'float64')
+
+print("Recording")
+sd.wait()
+print("recording complete")
+#sd.play(recording, sample_rate)
+
+f = open(filename, 'wb')
+wav.write(filename, sample_rate, recording)
+f.close()
+
+
+#sd.wait()
+print("Success")
